92415ecb0e703c1dfa724989f999f9f4a47a8392
Ruan Bahia <ruanmed@live.com>
2018-02-08 09:06:37 -0300

    Arquivo inicial

---
+++ src/labirinto-opengl.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : labirinto-opengl.cpp
+// Author      : Ruan Bahia
+// Version     :
+// Copyright   : 
+// Description : Jogo de labirinto feito com OpenGL
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
